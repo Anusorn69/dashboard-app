f2669e373eff3c4d97b629ad0bd50dce1a1bf727
Anusorn69 <kuanusorn@hotmail.com>
2023-05-15 15:12:57 +0700

    first commit

---
+++ database/migrations/2023_05_15_072817_create_dashboard_transections_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dashboard_transections', function (Blueprint $table) {
+            $table->id();
+            $table->integer('dashboard_id');
+            $table->integer('data_type_id');
+            $table->decimal('data_value',12,2);
+            $table->smallinteger('data_year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dashboard_transections');
+    }
+};
